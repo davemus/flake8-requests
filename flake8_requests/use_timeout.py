3e30efe2ba2552db29503d813a23e64a3cc2a386
grayson <grayson@returntocorp.com>
2019-11-21 16:53:27 -0800

    Refactored this plugin to have a single entrypoint.
    Previously, there were runners for each Visitor; now there is one runner for all Visitors. This unifies the presentation when examining the installed plugins.

--- flake8_requests/use_timeout.py
+++ flake8_requests/use_timeout.py
@@ -10,30 +10,8 @@ handler = logging.StreamHandler(stream=sys.stderr)
 handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
 logger.addHandler(handler)
 
-class UseTimeout(object):
-    name = "r2c-requests-use-timeout"
-    version = __version__
-
-    def __init__(self, tree):
-        self.tree = tree
-
-    def run(self):
-        visitor = UseTimeoutVisitor()
-        visitor.visit(self.tree)
-
-        for report in visitor.report_nodes:
-            node = report['node']
-            yield (
-                node.lineno,
-                node.col_offset,
-                self._message_for(),
-                self.name,
-            )
-
-    def _message_for(self):
-        return f"{self.name} use a timeout; requests will hang forever without a timeout (recommended 60 sec)"
-
 class UseTimeoutVisitor(RequestsBaseVisitor):
+    name = "r2c-requests-use-timeout"
 
     def visit_Call(self, call_node):
         logger.debug(f"Visiting Call node: {ast.dump(call_node)}")
@@ -55,22 +33,5 @@ class UseTimeoutVisitor(RequestsBaseVisitor):
         logger.debug(f"Found this node: {ast.dump(call_node)}")
         self.report_nodes.append({
             "node": call_node,
+            "message": f"{self.name} requests will hang forever without a timeout. Consider adding a timeout (recommended 10 sec)."
         })
-
-if __name__ == "__main__":
-    import argparse
-
-    logger.setLevel(logging.DEBUG)
-
-    parser = argparse.ArgumentParser()
-    # Add arguments here
-    parser.add_argument("inputfile")
-
-    args = parser.parse_args()
-
-    logger.info(f"Parsing {args.inputfile}")
-    with open(args.inputfile, 'r') as fin:
-        tree = ast.parse(fin.read())
-
-    visitor = UseTimeoutVisitor()
-    visitor.visit(tree)
